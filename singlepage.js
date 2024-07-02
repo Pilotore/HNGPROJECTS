7b4ab08f247c5e5f7561b9d5807d1abd54f8f948
Oreoluwa <116044640+Pilotore@users.noreply.github.com>
2024-07-02 17:50:03 +0100

    Update singlepage.js

--- singlepage.js
+++ singlepage.js
@@ -2,7 +2,7 @@ window.onload = function(){
     function updateTime() {
         const now = new Date();
         const utcTime = now. toUTCString().slice(-12,-4);
-        document.querySelector(`[data-testid="currentTimeUTC"]`).innerText = utcTime;
+        document.querySelector('[data-testid="currentTimeUTC"]').innerText = utcTime;
         // img.width = img.naturalWidth;
         // img.height = img.naturalHeight;
     }
@@ -12,5 +12,5 @@ window.onload = function(){
 
     const daysOfWeek =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDay =daysOfWeek[new Date().getUTCDay()];
-    document.querySelector(`[data-testid="currentDay"]`).innerText = currentDay;
+    document.querySelector('[data-testid="currentDay"]').innerText = currentDay;
 };
