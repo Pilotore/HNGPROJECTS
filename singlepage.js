595f71a234369db9e7927ab1c489cdeb5b44db25
Oreoluwa <oniretiore.2003@gmail.com>
2024-07-02 09:21:40 +0100

    Initial commit

---
+++ singlepage.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    function updateTime() {
+        const now = new Date();
+        const utcTime = now. toUTCString().slice(-12,-4);
+        document.querySelector(`[data-testid="currentTimeUTC"]`).innerText = utcTime;
+    }
+    updateTime();
+    setInterval(updateTime, 1000);
+
+
+    const daysOfWeek =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDay =daysOfWeek[new Date().getUTCDay()];
+    document.querySelector(`[data-testid="currentDay"]`).innerText = currentDay;
+}
